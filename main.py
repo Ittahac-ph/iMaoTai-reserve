44aad462a191148fb25f8439043765bbf2d49762
Ittahac-ph <53427972+Ittahac-ph@users.noreply.github.com>
2024-01-11 16:52:57 +0800

    Update main.py
    阿松大

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import login
 import process
 import privateCrypt
 
-DATE_FORMAT = "%m/%d/%Y %H:%M:%S %p"
+DATE_FORMAT = "%m/%d/%Y %H:%M:%S %p" 
 TODAY = datetime.date.today().strftime("%Y%m%d")
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s  %(filename)s : %(levelname)s  %(message)s',  # 定义输出log的格式
